fcc5aefaedad3ccb2dee17d77b51c9759d3b85e9
moongua404 <hogun.soongit@gmail.com>
2025-06-23 23:26:00 +0900

    feat: project-setup

---
+++ src/test/java/mission/ApplicationTest.java
@@ -0,0 +1,22 @@
+package mission;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import api.Application;
+import api.TestEnvironment;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest extends TestEnvironment {
+    @Test
+    void testApplication() {
+        run(List.of("250000", "1, 3, 10, 11, 15, 16"));
+        assertTrue(output().contains("예산을 초과했습니다."));
+        assertTrue(output().contains("3,000원"));
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
